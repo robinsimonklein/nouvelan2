c37e562124bdf0ec5ebb17da691996a311908893
Robin Simonklein <rsimonklein@gmail.com>
2019-12-25 19:44:43 +0100

    Add new https server start

--- server/index.js
+++ server/index.js
@@ -1,51 +1,75 @@
-const express = require('express')
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
-const isProd = process.env.NODE_ENV === 'production'
-const port = process.env.PORT || 3000
+
+// Create express app
+const app = express();
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
-config.dev = !isProd
+config.dev = process.env.NODE_ENV !== 'production'
 
-if (!isProd) {
-    nuxt.build() // Build in development mode
+if(!config.dev) {
+// Certificate
+    const privateKey = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/privkey.pem', 'utf8');
+    const certificate = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/cert.pem', 'utf8');
+    const ca = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/chain.pem', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
 }
+
 
 // Static config
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 
-// HTTPS Server
-const options = {
-    key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-    cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
+
+// Start function
+async function start() {
+    // Init Nuxt.js
+    const nuxt = new Nuxt(config)
+
+    const { host, port } = nuxt.options.server
+
+    // Build only in dev mode
+    if (config.dev) {
+        const builder = new Builder(nuxt)
+        await builder.build()
+    } else {
+        await nuxt.ready()
+    }
+
+    // Give nuxt middleware to express
+    app.use(nuxt.render)
+
+    // Starting both http & https servers
+    const httpServer = http.createServer(app);
+    if(!config.dev) {
+        const httpsServer = https.createServer(credentials, app);
+    }
+
+    httpServer.listen(process.env.PORT || 80, () => {
+        consola.ready({
+            message: `Server listening on http://${host}:${port}`,
+            badge: true
+        })
+    });
+
+    if(!config.dev) {
+        httpsServer.listen(process.env.HTTPS_PORT || 443, () => {
+            consola.ready({
+                message: `HTTPS Server listening on http://${host}:${port}`,
+                badge: true
+            })
+        });
+    }
 }
 
-async function start () {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
-
-  const { host, port } = nuxt.options.server
-
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-
-  https
-      .create(options, app)
-      .listen(port)
-
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
-}
 start()
