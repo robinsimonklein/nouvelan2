617208d97d5361077e175b2fc389bfa9899a3cc7
Robin Simonklein <rsimonklein@gmail.com>
2019-12-25 18:19:09 +0100

    test https

--- server/index.js
+++ server/index.js
@@ -1,37 +1,35 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
 
-// Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 
-// Static config
-app.use(express.static(__dirname, { dotfiles: 'allow' } ));
+const app = express();
 
-async function start () {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/nouvelan.ynibling.ovh/chain.pem', 'utf8');
 
-  const { host, port } = nuxt.options.server
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
 
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  } else {
-    await nuxt.ready()
-  }
+app.use((req, res) => {
+    res.send('Hello there !');
+});
 
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
-}
-start()
+httpServer.listen(80, () => {
+    console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
