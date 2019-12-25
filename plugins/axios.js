997552681192e13476a65d7d61558afaaeb1ce1a
Robin Simonklein <rsimonklein@gmail.com>
2019-12-25 22:48:33 +0100

    Fix axios

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,7 @@ let options = {}
 
 // The server-side needs a full url to works
 if (process.server) {
-  options.baseURL = `https://${process.env.HOST || 'localhost'}:${process.env.PORT || 3000}`
+  options.baseURL = `https://${process.env.HOST || 'localhost'}:${process.env.HTTPS_PORT || process.env.PORT || 3000}`
 }
 
 export default axios.create(options)
