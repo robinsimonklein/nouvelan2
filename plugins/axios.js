cd2394bad8cbdb1bc921865d43141cae7296ab18
Robin Simonklein <rsimonklein@gmail.com>
2019-12-27 12:17:58 +0100

    Fix axios in prod

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,8 +3,8 @@ import * as axios from 'axios'
 let options = {}
 
 // The server-side needs a full url to works
-if(process.env.NODE_ENV === 'production') {
-    options.baseURL = `${process.env.HTTPS_PORT ? 'https' : 'http'}://${process.env.HOST || 'localhost'}:${process.env.HTTPS_PORT || process.env.PORT || 3000}`
+if (process.env.NODE_ENV === 'production') {
+  options.baseURL = `https://${process.env.HOST || 'localhost'}:${process.env.HTTPS_PORT || process.env.PORT || 3000}`
 }
 
 export default axios.create(options)
