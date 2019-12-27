0f48fceb5d89e2aaef45bc6e8ebb9162778d8ea1
Robin Simonklein <rsimonklein@gmail.com>
2019-12-27 12:27:11 +0100

    Fix axios f'tomi

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,7 @@ let options = {}
 
 // The server-side needs a full url to works
 if (process.env.NODE_ENV === 'production') {
-  options.baseURL = `https://${process.env.HOST || 'localhost'}:${process.env.HTTPS_PORT || process.env.PORT || 3000}`
+  options.baseURL = `https://${process.env.AXIOS_HOST || 'localhost'}:${process.env.HTTPS_PORT || process.env.PORT || 3000}`
 }
 
 export default axios.create(options)
